520276b71f2f64f5862d4a605eb7a2a25a8cb567
shanto <shantokumar22@gmail.com>
2026-07-27 19:04:11 +0600

    Module 13 solve 16 complete

--- Milestone2/Module14/index.js
+++ Milestone2/Module14/index.js
@@ -81,3 +81,99 @@ function canAttendFinalExam(attendance, assignmentMark){
 
 console.log(canAttendFinalExam(90, 80));
 
+
+
+console.log("=======================");
+console.log("=======================");
+
+// =========== Bug four ===========
+// =========== Bug four ===========
+
+
+// return the sum of all number from 1 to n (inclusive)
+function sumRange(n){
+    for(let i=1; i < n ; i++){ // only use less than  . but we should use less than or equal <=
+        let total = 0; // we cannot defined variable in the loop
+        total += i;
+        return total; // we cannot use return in the loop
+    }
+
+}
+
+console.log(sumRange(5));
+
+
+console.log("=======================");
+console.log("=======================");
+
+// =========== Bug five ===========
+// =========== Bug five ===========
+
+// return the sum of all number in the array
+
+function sumArry(num){
+    let total= 0;
+    for(let i = 0; i <= num.length; i++){  // akhane <= dewa te 0 1 2 3 ei 4ta value print hobe jar ses value undefined then jokhon undefined.toFixed(2) hobe tkhn e error. solve hocce i < num.lenght.
+
+        total += num[i].toFixed(2)   //toFixed use korle string e convert hoy. tai Number e wrap kore number e conver kore dite hobe Number(num[i].toFixed(2))
+    }
+    return total
+}
+
+// console.log(sumArry([10,20,20.2244 ]));
+
+console.log("=======================");
+console.log("=======================");
+
+// =========== Bug six ===========
+// =========== Bug six ===========
+
+
+// count how many even numbers are present in the array
+
+
+function countEven(num){
+    let count = 0;
+    for(let i = 0; i < num.length; i++){ //akhane comma use kora jabe na semicolne use korte hobe
+        if(num[i] % 2 === 0){
+            count++;
+            i++ // ei i++ remove kore dite hobe 
+        }
+    }
+    return count;
+}
+
+console.log(countEven([2,5,4,7,8,10]));
+
+
+
+console.log("=======================");
+console.log("=======================");
+
+// =========== Bug seven ===========
+// =========== Bug seven ===========
+
+// problem one: getLastTwo(arr)
+// purpose: should return the last 2 elements of the array as a new array 
+
+//function getLastTwo(arr)={ // function e equal use kora jay na
+    //return arr.slice(arr.length -1 , arr.length ) // ekhne length -2  hobe last duita element dorte gele
+    //return result.toUpperCase(); // 2nd time return not working
+// }
+
+// console.log(getLastTwo([10,20,30,40]));
+
+
+//  Problem 2: findIndexOfValue(arr, target)
+//  purpose: should return the index of target inside arr, or -1 if not found.
+
+function findIndexOfValue(arr, target){
+    for(let i = 0; i <=arr.length; i++){
+        if(arr[i].toString() === target.toString()){
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(findIndexOfValue([10,20,30], 10));

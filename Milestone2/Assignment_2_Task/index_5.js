633cc020c08960d44590ae4428476bdbee864e42
shanto <shantokumar22@gmail.com>
2026-07-29 18:42:21 +0600

    Assignment solve 5 complete

---
+++ Milestone2/Assignment_2_Task/index_5.js
@@ -0,0 +1,42 @@
+// A-04  —  Palindrome Checker         🟡 Medium
+
+
+
+// Concepts Used:  Strings · Reversal
+// Scenario:  Write a function isPalindrome(str) that returns true if the string reads the same forwards and backwards (e.g. 'madam').
+// If the input is not a string, return "Invalid".
+
+// Expected Output:
+//    isPalindrome('madam') → true
+//    isPalindrome('hero')  → false
+
+
+// Hints:
+//   1.  Reverse the string using any method from Session 2
+//   2.  Compare the reversed version to the original with ==
+
+
+function isPalindrome(str){
+
+    if(typeof str !== 'string' || str.length === 0){
+        return "Invalid"
+    }
+
+    let reversed = ''
+
+    for(let i = str.length - 1; i >= 0; i-- ){
+        reversed += str[i]
+    }
+    if(reversed === str){
+       return true
+    }else{
+        return false
+    }
+    
+    
+    
+
+}
+
+console.log(isPalindrome('madam'));
+console.log(isPalindrome('hero'));

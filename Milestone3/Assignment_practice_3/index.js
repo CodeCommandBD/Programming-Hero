2f13fb12014add1b749854d8b30dd43f8683fb2b
shanto <shantokumar22@gmail.com>
2026-08-08 09:30:33 +0600

    Assignment 3 solve 2

--- Milestone3/Assignment_practice_3/index.js
+++ Milestone3/Assignment_practice_3/index.js
@@ -1,13 +1,3 @@
-// Problem 1 — Discount Total Calculator
-// একটি Shop-এ প্রতিটি Product-এর Price একটি Number Array আকারর দেওযা আরে। প্ররিেক Price দেরক ৫ িাকা
-// Discount তেরয, সব Updated Price দ াগ করর Total Return কররি হরব।
-// Input
-// [100, 250, 80]
-// Output
-// 415
-// Explanation
-// Updated Prices: [95, 245, 75] → Total: 95 + 245 + 75 = 415
-
 
 const DiscountTotalCalculator = (prices, discount = 5) =>{
      

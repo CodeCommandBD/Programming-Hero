bb85d28f19f2db6dc72d4c331a06e3809ebff71f
shanto <shantokumar22@gmail.com>
2026-08-16 11:01:11 +0600

    Module 21 Task

---
+++ Milestone4/Module21/Task/problem_6.js
@@ -0,0 +1,6 @@
+var isEven = function (num) {
+    if (num % 2 === 0) {
+        return true;
+    }
+};
+console.log(isEven(8));
---
+++ Milestone4/Module21/Task/problem_6.ts
@@ -0,0 +1,8 @@
+
+const isEven =(num:number)=>{
+    if(num % 2 === 0){
+        return true
+    }
+}
+
+console.log(isEven(8));

582f4d0f084ec5ceb83ddc4bd97938343b38dde1
shanto <shantokumar22@gmail.com>
2026-08-16 10:52:58 +0600

    Module 21 Task

--- Milestone4/Module21/Task/problem_5.js
+++ Milestone4/Module21/Task/problem_5.js
@@ -1,4 +1,4 @@
 var info = function (data) {
-    return "Name: ".concat(data.name);
+    return "Name: ".concat(data.name, " Email: ").concat(data.email || "Not provided", " ");
 };
 console.log(info({ name: 'shnto' }));
--- Milestone4/Module21/Task/problem_5.ts
+++ Milestone4/Module21/Task/problem_5.ts
@@ -1,6 +1,8 @@
 
-const info = (data:{name: string})  => {
-    return `Name: ${data.name}`
+const info = (data:{name: string, email?: string})  => {
+    
+    return `Name: ${data.name} Email: ${data.email || "Not provided"} `
+
 }
 
 console.log(info({name:'shnto'}));

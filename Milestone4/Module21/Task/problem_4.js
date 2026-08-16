28abeadb2adb7f9fa546408f586a3a13fb858861
shanto <shantokumar22@gmail.com>
2026-08-16 10:28:23 +0600

    Module 21 Task

---
+++ Milestone4/Module21/Task/problem_4.js
@@ -0,0 +1,4 @@
+var student = function (data) {
+    return "".concat(data[0], " is ").concat(data[1], " years old");
+};
+console.log(student(['shanto', 22]));
---
+++ Milestone4/Module21/Task/problem_4.ts
@@ -0,0 +1,8 @@
+const student = (data: [string, number]) => {
+    return `${data[0]} is ${data[1]} years old`
+    
+}
+
+
+
+console.log(student(['shanto' , 22]));

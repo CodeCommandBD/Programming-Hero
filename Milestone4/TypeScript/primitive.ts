4d26abbe7f7853bb86ac3f68a2a39629373cceba
shanto <shantokumar22@gmail.com>
2026-08-17 09:33:45 +0600

    TypeScript

---
+++ Milestone4/TypeScript/primitive.ts
@@ -0,0 +1,11 @@
+let district: string = "Downtown";
+let population: number = 50000;
+let isCapital: boolean = true;
+let undefinedValue: undefined;
+
+district = "Uptown";
+
+console.log(`District: ${district}`);
+console.log(`Population: ${population}`);
+console.log(`Is Capital: ${isCapital}`);
+console.log(`Undefined Value: ${undefinedValue}`);

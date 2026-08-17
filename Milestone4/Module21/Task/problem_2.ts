56fcfaf254f409fc3688150f8eda0d5946083c86
shanto <shantokumar22@gmail.com>
2026-08-17 11:54:52 +0600

    Module 21 Task

---
+++ Milestone4/Module21/Task/problem_2.ts
@@ -0,0 +1,6 @@
+const productsName: string = "Laptop";
+const productsPrice: number = 1200;
+const isAvailable: boolean = true;
+
+
+console.log(`${productsName} cost ${productsPrice} and is available: ${isAvailable}`);

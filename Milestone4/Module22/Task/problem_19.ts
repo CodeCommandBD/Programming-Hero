26692e12d998b2f1ca9a6169f5a187049e4f9280
shanto <shantokumar22@gmail.com>
2026-08-19 08:14:21 +0600

    Module 21  22 Task

---
+++ Milestone4/Module22/Task/problem_19.ts
@@ -0,0 +1,18 @@
+// Problem statement: Create an appConfig object (theme, version), lock it with as const. Try mutating a property and comment on the resulting error. Derive a union type from an array using typeof + as const.
+// Input: const appConfig = { theme: "dark", version: 2 } as const;
+// Output: commented error on mutation attempt (Cannot assign to 'theme' because it is a read-only property); derived union type example (e.g. "red" | "green" | "blue").
+
+
+const appConfig = {
+    theme: 'dark',
+    version: 2
+} as const
+
+// appConfig.theme = "light" 
+
+
+const colors = ['red', 'green', 'blue'] as const 
+
+type ColorType = typeof colors[number]
+
+const myfav: ColorType = 'red'

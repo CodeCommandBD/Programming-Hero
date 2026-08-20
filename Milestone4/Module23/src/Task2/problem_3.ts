1505fe61d86ee0fa24cad199babace924766d39a
shanto <shantokumar22@gmail.com>
2026-08-21 02:29:01 +0600

    Module 24

---
+++ Milestone4/Module23/src/Task2/problem_3.ts
@@ -0,0 +1,15 @@
+interface Player{
+    username: string;
+    level: number;
+    region: string
+}
+const formatPlayerCard = (player: Player):string =>{
+    return `${player.username} is a level ${player.level} player from ${player.region}`
+}
+
+console.log(formatPlayerCard({
+    username: "ShadowByte",
+    level: 42,
+    region: "Asia"
+}
+));

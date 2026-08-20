6e5aedf3e79fb03a8f3bdd965ff7b4a5f5d981d8
shanto <shantokumar22@gmail.com>
2026-08-20 13:17:22 +0600

    Module 24

---
+++ Milestone4/Module23/src/task/problem_7.ts
@@ -0,0 +1,23 @@
+interface Product {
+    name: string;
+    price: number;
+    category: string
+}
+
+const findProducts = (product: Product[], category:string ): Product[] => {
+    const productCategory = product.filter((item)=>{
+        return item.category === category
+    })
+
+    return productCategory
+}
+
+const products = [
+    { name: "iPhone 15", price: 90000, category: "phone" },
+    { name: "Galaxy S24", price: 85000, category: "phone" },
+    { name: "MacBook Air", price: 120000, category: "laptop" },
+    { name: "Dell XPS", price: 110000, category: "laptop" }
+];
+ 
+console.log(findProducts(products, "phone"));
+

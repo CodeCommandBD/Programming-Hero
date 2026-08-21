3e5aac04ce6e70e0631c9acd3c952ce015b23977
shanto <shantokumar22@gmail.com>
2026-08-21 08:54:18 +0600

    Module 24

---
+++ Milestone4/Module23/src/Task2/problem_8.ts
@@ -0,0 +1,27 @@
+interface LikeNotification {
+    type: 'like';
+    fromUser: string
+}
+interface SystemNotification {
+    type: 'system';
+    message: string
+    actionUrl?: string
+}
+
+const  renderNotification = (notification: LikeNotification | SystemNotification):string => {
+    if(notification.type === 'like'){
+        return `${notification.fromUser} liked your post.`
+    }
+
+    if(notification.actionUrl){
+        return `System: ${notification.message} (Tap to view)`;
+    }
+    
+    return `System: ${notification.message}`
+    
+    
+}
+
+console.log(renderNotification({ type: "like", fromUser: "Aisha" }));
+console.log(renderNotification({ type: "system", message: "Maintenance complete." }));
+console.log(renderNotification({ type: "system", message: "Your subscription is expiring soon.", actionUrl: "/billing" }));

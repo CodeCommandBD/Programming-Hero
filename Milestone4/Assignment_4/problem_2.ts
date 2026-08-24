a5cde59cbc24e131990457fbab3e20da9e0ab5a7
shanto <shantokumar22@gmail.com>
2026-08-24 18:50:50 +0600

    Milestone 4 Exam

---
+++ Milestone4/Assignment_4/problem_2.ts
@@ -0,0 +1,12 @@
+type Booking = {
+    name: string;
+    guests: number;
+    time: string
+}
+
+
+const formatBookingConfirmation = (booking: Booking):string =>{
+    return `${booking.name}'s table for ${booking.guests} guests is confirmed at ${booking.time}.`
+}
+
+console.log(formatBookingConfirmation({ name: "Aisha", guests: 4, time: "7:00 PM" }));
